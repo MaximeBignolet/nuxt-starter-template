37bf811f2ff253c5006fbcc46bf6a4cc7fc2c4eb
Maxime Bignolet <maxime.bignolet@rhinos-groupe.com>
2024-08-28 10:39:43 +0200

    feat(): update .eslint to eslingconfig.mjs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2024-04-03',
-	ssr: false,
-	devtools: { enabled: true },
-	modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
-	eslint: {
-		config: {
-			stylistic: {
-				indent: 'tab',
-				semi: true,
-				jsx: true,
-			},
-		},
-	},
-	components: [
-		{
-			path: '~/components',
-			pathPrefix: false,
-		},
-	],
-	imports: {
-		autoImport: true,
-	},
+  compatibilityDate: `2024-04-03`,
+  ssr: false,
+  devtools: { enabled: true },
+  modules: [ `@nuxtjs/tailwindcss`, `@nuxt/eslint` ],
+  components: [
+    {
+      path: `~/components`,
+      pathPrefix: false
+    }
+  ],
+  imports: {
+    autoImport: true
+  }
 });
